eb5d2d777d4e5ca4c2e78bc977f9b2275ffe26db
aitor-g <114782818+aitor-g@users.noreply.github.com>
2023-09-21 16:21:44 +0100

    Get the full structure of the game

--- js/main.js
+++ js/main.js
@@ -2,18 +2,76 @@ function getComputerChoice() {
   let selection = Math.ceil(Math.random() * 3);
   switch (selection) {
     case 1:
-      selection = "Rock";
-      break;
+      return selection = "Rock";
     case 2:
-      selection = "Paper";
-      break;
+      return selection = "Paper";
     case 3:
-      selection = "Scissors";
-      break;
-    default:
-      break;
+      return selection = "Scissors";
   }
-  return selection;
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+
+  switch (playerSelection) {
+    case "Rock":
+      switch (computerSelection) {
+        case "Scissors":
+          return 2;
+        case "Paper":
+          return 1;
+        default:
+          return 0;
+      }
+
+    case "Paper":
+      switch (computerSelection) {
+        case "Rock":
+          return 2;
+        case "Scissors":
+          return 1;
+        default:
+          return 0;
+      }
+
+    case "Scissors":
+      switch (computerSelection) {
+        case "Paper":
+          return 2;
+        case "Rock":
+          return 1;
+        default:
+          return 0;
+      }
+  }
+}
+
+function game() {
+  let winsCounter = 0;
+  let losesCounter = 0;
+
+  for (let index = 0; index < 5; index++) {
+    let playerSelection = prompt("Introduce Paper, Scissors or Rock:").toLowerCase();
+    playerSelection = playerSelection.replace(playerSelection.charAt(0), playerSelection.charAt(0).toUpperCase());
+    let computerSelection = getComputerChoice();
+    let roundResult = playRound(playerSelection, computerSelection);
+    if (roundResult === 2) {
+      winsCounter++;
+      console.log(`You Win the Round! Your ${playerSelection} beats computer's ${computerSelection}!`);
+    } else if (roundResult === 1) {
+      losesCounter++;
+      console.log(`You Lose the Round! Computer's ${computerSelection} beats your ${playerSelection}!`);
+    } else {
+      console.log("This Round ends in a Draw!");
+    }
+  }
+
+  if (winsCounter > losesCounter) {
+    console.log(`You win the game for ${winsCounter} victories to ${losesCounter}!`)
+  } else if (winsCounter < losesCounter) {
+    console.log(`You lose the game for ${winsCounter} victories to ${losesCounter}!`)
+  } else {
+    console.log(`The game end in a ${winsCounter} victories Draw!`)
+  }
+}
+
+game();
